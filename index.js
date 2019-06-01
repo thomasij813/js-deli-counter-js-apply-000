c6c9f7ebd958723c81e2f312a70936f142e5d0c3
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:51:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentLine, newCustomerName) {
-  currenLine.push(newCustomerName)
+  currentLine.push(newCustomerName)
   return `Welcome, ${newCustomerName}. You are number ${currentLine.length} in line.`
 }
