1df8b6a923b2e2926f25b399a6f615558f991f45
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:59:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(currentLine) {
   if (currentLine.length === 0) {
     return 'There is nobody waiting to be served!'
   } else {
-    return `Currently serving ${currentLine.shift()}`
+    return `Currently serving ${currentLine.shift()}.`
   }
 }
 
