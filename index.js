b8c7d99997f8f05c09317f8d5333b2a47af750ee
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:58:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function nowServing(currentLine) {
     return `Currently serving ${currentLine.shift()}`
   }
 }
+
+function currentLine(currentLine) {
+  if (currentLine.length === 0) {
+    return 'The line is currently empty'
+  }
+  
+  let outputString = 'The line is currently: '
+  currentLine.forEach(function(customerName, i) {
+    const placeInLine = i + 1
+    const comma = placeInLine === currentLine.length ? '' : ','
+    outputString += `${placeInLine}. ${customerName}${comma}` 
+  })
+  
+  return outputString
+}
