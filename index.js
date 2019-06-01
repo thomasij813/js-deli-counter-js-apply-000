dce1d52df8f7d733d5eb54962816c2cc789f0f32
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(currentLine) {
 
 function currentLine(currentLine) {
   if (currentLine.length === 0) {
-    return 'The line is currently empty'
+    return 'The line is currently empty.'
   }
   
   let outputString = 'The line is currently: '
