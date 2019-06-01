82349e6d11f6ff0b5179247b8f041b2b8ca50d97
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(currentLine, newCustomerName) {
   currentLine.push(newCustomerName)
   return `Welcome, ${newCustomerName}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(currentLine) {
+  if (currentLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+}
